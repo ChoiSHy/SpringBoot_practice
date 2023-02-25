1d7abcbf2364ead54f08f2f478ec62479024263f
ChoiSHy <tjdgus4697@gmail.com>
2023-02-26 04:51:46 +0900

    swagger 추가 Logger 추가

--- api/src/main/java/com/springboot/api/config/SwaggerConfiguration.java
+++ api/src/main/java/com/springboot/api/config/SwaggerConfiguration.java
@@ -19,7 +19,7 @@ public class SwaggerConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.spring.api"))
+                .apis(RequestHandlerSelectors.basePackage("com.springboot.api"))
                 .paths(PathSelectors.any())
                 .build();
     }
--- api/src/main/java/com/springboot/api/controller/GetController.java
+++ api/src/main/java/com/springboot/api/controller/GetController.java
@@ -1,6 +1,10 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dto.MemberDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -8,53 +12,59 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
 
     // http://localhost:8080/api/v1/get-api/hello
-    @RequestMapping(value = "/hello", method= RequestMethod.GET)
-    public String getHello(){
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        LOGGER.info("getHello 메서드 호출");
         return "Hello, world...";
     }
 
     // http://localhost:8080/api/v1/get-api/name
-    @GetMapping (value="/name")
-    public String getName(){
+    @GetMapping(value = "/name")
+    public String getName() {
+        LOGGER.info("getName 메서드 호출");
         return "ChoiSHy";
     }
 
     // http://localhost:8080/api/v1/get-api/variable1/변수
     @GetMapping(value = "/variable1/{variable}")
-    public String getVariable1(@PathVariable String variable){
+    public String getVariable1(@PathVariable String variable) {
+        LOGGER.info("@PathVariable을 통해 들어온 값 : {}", variable);
         return variable;
     }
 
     // http://localhost:8080/api/v1/get-api/variable2/변수
     @GetMapping(value = "/variable2/{variable}")
-    public String getVariable2(@PathVariable("variable") String var){
+    public String getVariable2(@PathVariable("variable") String var) {
         return var;
     }
 
-    // http://localhost:8080/api/v1/get-api/request1?name=이름&email=이메일&org=소속
-    //@ApiOperation(value="GET 메소드 예제", notes="@RequestParam을 활용한 GET Method")
+    // http://localhost:8080/api/v1/get-api/request1?name=이름&email=이메일&org=회사
+    @ApiOperation(value = "GET 메소드 예제", notes = "@RequestParam을 활용한 GET 메소드")
     @GetMapping(value = "/request1")
     public String getRequestParam1(
-            @RequestParam String name,
-            @RequestParam String email,
-            @RequestParam String org){
-        return name+" "+email+" "+org;
+            @ApiParam(value = "이름", required = true) @RequestParam String name,
+            @ApiParam(value = "이메일", required = true) @RequestParam String email,
+            @ApiParam(value = "회사", required = true) @RequestParam String org) {
+        return name + " " + email + " " + org;
     }
+
     // http://localhost:8080/api/v1/get-api/request2?key1=val1&key2=val2&key3=val3
     @GetMapping(value = "/request2")
-    public String getRequestParam2(@RequestParam Map<String, String> param){
-        StringBuilder sb= new StringBuilder();
+    public String getRequestParam2(@RequestParam Map<String, String> param) {
+        StringBuilder sb = new StringBuilder();
 
-        param.entrySet().forEach(map->{
-            sb.append(map.getKey() + " : "+ map.getValue()+"\n");
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
         });
         return sb.toString();
     }
+
     // http://localhost:8080/api/v1/get-api/request3?name=이름&email=이메일&org=소속
     @GetMapping(value = "/request3")
-    public String getRequestParam3(MemberDto memberDto){
+    public String getRequestParam3(MemberDto memberDto) {
         return memberDto.toString();
     }
 }
